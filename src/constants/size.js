adcf66ba41f127b0e936833c6016a291d0c38748
김용민 <dydals3440@gmail.com>
2023-12-02 01:14:30 +0900

    feat: font-weight 상수 추가

--- src/constants/size.js
+++ src/constants/size.js
@@ -1,7 +1,7 @@
 const RESPONSIVE_SIZE = {
   MOBILE: '600px',
   TABLET: '768px',
-  PC: '1024px',
+  PC: '1024px'
 };
 
 const FONT_SIZE = {
@@ -15,7 +15,19 @@ const FONT_SIZE = {
   FOUR_XL: '2.25rem',
   FIVE_XL: '3rem',
   SIX_XL: '3.75rem',
-  SEVEN_XL: '4.5rem',
+  SEVEN_XL: '4.5rem'
 };
 
-export { RESPONSIVE_SIZE, FONT_SIZE };
+const FONT_WEIGHT = {
+  THIN: 100,
+  EXTRA_LIGHT: 200,
+  LIGHT: 300,
+  NORMAL: 400,
+  MEDIUM: 500,
+  SEMI_BOLD: 600,
+  BOLD: 700,
+  EXTRA_BOLD: 800,
+  FONT_BLACK: 900
+};
+
+export { RESPONSIVE_SIZE, FONT_SIZE, FONT_WEIGHT };
