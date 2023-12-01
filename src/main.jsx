c1506b30b418902c9c98e23bb332086aa787f0ef
daindaind <https://github.com/daindaind>
2023-12-01 21:54:57 +0900

    theme color 수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App';
 import './index.css';
 import { store } from '../src/redux/store/store.js';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from "styled-components";
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
   </Provider>,
 );
---
+++ src/theme.jsx
@@ -0,0 +1,22 @@
+const color = {
+    primary: {
+        blue: "#4a6bf9",
+        bluePale: "#b0c2fc",
+        blueLight: "#9bb4ee",
+        blueSecondary: "#eef3fe",
+    },
+    neutral: {
+        black: "#000000",
+        white: "#ffffff",
+        boldTitle: "#232731",
+        title: "#2f2f2f",
+        content: "#74757b",
+        contentBg: "#f6f6f7",
+    },
+    yellow: "#ffca3d",
+}
+
+
+const theme = { color };
+
+export default theme;
