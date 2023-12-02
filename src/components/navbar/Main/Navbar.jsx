8112c58b7dc81efd782ad82fc3ec8a9bd7be9446
김용민 <dydals3440@gmail.com>
2023-12-02 16:44:33 +0900

    fix: vite환경 mui optimizeDeps 해결

--- src/components/navbar/Main/Navbar.jsx
+++ src/components/navbar/Main/Navbar.jsx
@@ -1,7 +1,14 @@
+import App from '../../../App';
 import * as S from './Navbar.style';
+import { AppBar, TextField } from '@mui/material';
 
 const Navbar = () => {
-  return <S.NavWrapper>Navbar</S.NavWrapper>;
+  return (
+    <>
+      <S.NavWrapper>123</S.NavWrapper>;
+      <TextField />
+    </>
+  );
 };
 
 export default Navbar;
