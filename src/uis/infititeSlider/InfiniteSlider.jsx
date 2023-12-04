6e37bb84052498880fa3e5e31f188573d91dcbff
chaKK <cdin23@naver.com>
2023-12-05 00:14:56 +0900

    InfiniteSlider 구현

--- src/uis/infititeSlider/InfiniteSlider.jsx
+++ src/uis/infititeSlider/InfiniteSlider.jsx
@@ -1,40 +1,46 @@
 import * as S from './InfiniteSlider.style';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
 import { SliderCard } from '../../components';
+import { useState } from 'react';
 
-const InfiniteSlider = ({
-  cardWidth,
-  cardHeight,
-  dataList,
-  slidesToShow = 3
-}) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: slidesToShow,
-    slidesToScroll: 3,
-    autoplay: true,
-    autoplaySpeed: 3500
-  };
+const InfiniteSlider = ({ cardWidth, cardHeight, dataList }) => {
+  const [animate, setAnimate] = useState(true);
+  const onStop = () => setAnimate(false);
+  const onRun = () => setAnimate(true);
+
   return (
     <S.Container>
-      <S.StyledSlider {...settings}>
-        {dataList.map((item, index) => {
-          return (
-            <S.ImageContainer key={index}>
-              <SliderCard
-                title={item.title}
-                src={item.src}
-                width={cardWidth}
-                height={cardHeight}
-              />
-            </S.ImageContainer>
-          );
-        })}
-      </S.StyledSlider>
+      <S.SlideContainer>
+        <S.SlideWrapper onMouseEnter={onStop} onMouseLeave={onRun}>
+          <S.Slide className={'original'.concat(animate ? '' : ' stop')}>
+            {dataList.map((item, index) => (
+              <S.CardContainer>
+                <SliderCard
+                  key={index}
+                  title={item.title}
+                  src={item.src}
+                  width={cardWidth}
+                  height={cardHeight}
+                />
+              </S.CardContainer>
+            ))}
+          </S.Slide>
+          <S.Slide className={'clone'.concat(animate ? '' : ' stop')}>
+            {dataList.map((item, index) => (
+              <S.CardContainer>
+                <SliderCard
+                  key={index}
+                  title={item.title}
+                  src={item.src}
+                  width={cardWidth}
+                  height={cardHeight}
+                />
+              </S.CardContainer>
+            ))}
+          </S.Slide>
+        </S.SlideWrapper>
+      </S.SlideContainer>
     </S.Container>
   );
 };
--- src/uis/infititeSlider/InfiniteSlider.style.js
+++ src/uis/infititeSlider/InfiniteSlider.style.js
@@ -1,18 +1,98 @@
-import styled from 'styled-components';
-import Slider from 'react-slick';
+import styled, { keyframes } from 'styled-components';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+const infiniteAnimation1 = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    50% {
+        transform: translateX(-100%);
+    }
+    50.1% {
+        transform: translateX(100%);
+    }
+    100% {
+        transform: translateX(0%);
+    }
+`;
+
+const infiniteAnimation2 = keyframes`
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-200%);
+    }
+`;
+
 const Container = styled.div``;
 
-const StyledSlider = styled(Slider)`
-  .slick-slide div {
-    outline: none;
+const SlideContainer = styled.div`
+  display: flex;
+  overflow: hidden;
+`;
+
+const SlideWrapper = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: nowrap;
+  position: relative;
+  padding: 40px 0;
+
+  &::before {
+    display: block;
+    width: 100%;
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  }
+  &.original {
+    animation: ${infiniteAnimation1} 80s linear infinite normal none running;
+  }
+  &.clone {
+    animation: ${infiniteAnimation2} 80s linear infinite;
+  }
+  &.stop {
+    animation-play-state: paused;
   }
 `;
 
-const ImageContainer = styled.div`
-  margin: 0 16px;
+const CardContainer = styled.div`
+  margin: 0 10px;
+  cursor: pointer;
+  z-index: 2;
+  transition: 0.3s;
+  transform: scale(1);
+  &:hover {
+    transform: scale(0.98);
+    &::after {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+  }
+  &.big {
+    width: 280px;
+    height: 280px;
+  }
+  &.small {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
-export { Container, StyledSlider, ImageContainer };
+export { Container, SlideContainer, SlideWrapper, Slide, CardContainer };
