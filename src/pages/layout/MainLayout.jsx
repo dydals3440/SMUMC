d20b17702f6f51af272c523486cb94e2ecbb9715
김용민 <dydals3440@gmail.com>
2023-12-12 03:52:42 +0900

    feat: 연말 snow confetti 추가

--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -1,13 +1,30 @@
-import * as S from './MainLayout.style';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer, Navbar, Sidebar } from '../../components';
 import { useSelector } from 'react-redux';
 
+import Confetti from 'react-confetti';
+
+import * as S from './MainLayout.style';
+import drawSnow from '../../hooks/drawSnow';
+
 const MainLayout = () => {
   const { isOpen } = useSelector(state => state.sidebar);
+  const height = '3000';
+  const width = document.documentElement.scrollWidth;
 
   return (
     <S.Wrapper>
+      <Confetti
+        wind={0.01}
+        width={width}
+        height={height}
+        opacity={0.6}
+        drawShape={drawSnow}
+        tweenDuration={8000}
+        numberOfPieces={200}
+        gravity={0.05}
+      />
       <Navbar />
       <Outlet />
       {isOpen && <Sidebar />}
