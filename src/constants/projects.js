ab1330ed72dab8902017949079677a6a24a5cb9c
김용민 <dydals3440@gmail.com>
2024-02-29 01:29:22 +0900

    feat: 5기 프로젝트 정보 추가

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -184,6 +184,164 @@ const PROJECTS = [
     stack: ['REACT', 'SPRING'],
     img: '/4year/healody/healody.png',
     release: 'https://www.makeus.in/3b82ef8f-8398-465f-9dc6-31f383742005'
+  },
+  {
+    id: 15,
+    name: '여행의 이유',
+    description:
+      '나를 알고 너를 알고 여행지를 알면, 막막한 여행도 지피지기 백전백승',
+    theme: ['WEB', 'NODE'],
+    github: 'https://github.com/Here-You',
+    year: 5,
+    member: ['매튜/김용민', '다라/차다인', '예닝/문예윤'],
+    stack: ['REACT', 'NODE'],
+    img: '/5year/여행의이유/여행의이유.png',
+    release: 'https://www.here-you.com/'
+  },
+  {
+    id: 16,
+    name: '고민친구',
+    description: '세상의 모든 고민이 거쳐가는 공간, 고민친구',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['데이빗/김태영', '데브/이지민', '구리/박상우'],
+    stack: ['REACT', 'SPRING'],
+    img: '/5year/고민친구/고민친구.png',
+    release: ''
+  },
+  {
+    id: 17,
+    name: 'AboutMe',
+    description: '다양한 나, 다양한 관계의 시작',
+    theme: ['ANDROID', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['파스텔/정서연', '다에몬/정승원', '하나/최가나', '렉스/변성호'],
+    stack: ['KOTLIN', 'SPRING'],
+    img: '/5year/AboutMe/AboutMe.png',
+    release: ''
+  },
+  {
+    id: 18,
+    name: 'Draw Desktop',
+    description:
+      '개성 있게 간편하면서 직관적으로 바탕 화면을 꾸밀 수 있는 요소를 제공',
+    theme: ['WEB', 'NODE'],
+    github: null,
+    year: 5,
+    member: ['루시퍼/박세현', '냥젤리/김훈', '뻔뻔/김건'],
+    stack: ['REACT', 'NODE'],
+    img: '/5year/DrawDesktop/DrawDesktop.png',
+    release: ''
+  },
+  {
+    id: 19,
+    name: 'Rhythm Palette',
+    description: '#음악공유 #이미지생성 #SNS',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['엘레나/권유정', '우현/이현우', '사하/윤근수'],
+    stack: ['REACT', 'SPRING'],
+    img: '/5year/RhythmPalette/RhythmPalette.png',
+    release: ''
+  },
+  {
+    id: 20,
+    name: 'Writeroom',
+    description: '자유로운 창작의 공간',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['레이튼/박지환', '주디/이연주'],
+    stack: ['REACT', 'SPRING'],
+    img: '/5year/Writeroom/Writeroom.png',
+    release: ''
+  },
+  {
+    id: 21,
+    name: '다온',
+    description: '암환자들을 위한 하루 기록 서비스',
+    theme: ['ANDROID', 'NODE'],
+    github: null,
+    year: 5,
+    member: ['검정/권혁찬', '하늘/강호준'],
+    stack: ['KOTLIN', 'NODE'],
+    img: '/5year/다온/다온.png',
+    release: ''
+  },
+  {
+    id: 22,
+    name: '더 굿즈',
+    description: '개인 판매자 대상 온라인 주문서 플랫폼',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['도톨이/하지수', '시로/박선균', '주니/김형준'],
+    stack: ['REACT', 'SPRING'],
+    img: '/5year/더굿즈/더굿즈.png',
+    release: ''
+  },
+  {
+    id: 23,
+    name: '아브아브',
+    description: '레크레이션 종합 플랫폼 서비스',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['제이미/김준환'],
+    stack: ['REACT', 'SPRING'],
+    img: '/5year/아브아브/아브아브.png',
+    release: ''
+  },
+  {
+    id: 24,
+    name: '메메',
+    description: '나만의 메이크업 메이트',
+    theme: ['IOS', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['딩동/이동현', '제이스/임재영', '요비/김승엽'],
+    stack: ['SWIFT', 'SPRING'],
+    img: '/5year/메메/메메.png',
+    release: ''
+  },
+  {
+    id: 25,
+    name: '마플',
+    description: '사용자 저장 장소 기반 기록 + 일정 관리 서비스 : 마플',
+    theme: ['IOS', 'NODE'],
+    github: null,
+    year: 5,
+    member: ['도요/김영준'],
+    stack: ['SWIFT', 'NODE'],
+    img: '/5year/마플/마플.png',
+    release: ''
+  },
+  {
+    id: 26,
+    name: '스포너스',
+    description: '기업과 대학생이 만나는 자리',
+    theme: ['IOS', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['가니/박가은', '니니/김유빈', '피딕/황인성', '세헌/호세헌'],
+    stack: ['SWIFT', 'SPRING'],
+    img: '/5year/스포너스/스포너스.png',
+    release: ''
+  },
+  {
+    id: 27,
+    name: '이사.ZIP',
+    description: '이사를 위한 정보',
+    theme: ['ANDROID', 'SPRING'],
+    github: null,
+    year: 5,
+    member: ['러츠/강다희', '마린/강바다'],
+    stack: ['KOTLIN', 'SPRING'],
+    img: '/5year/이사/이사.png',
+    release: ''
   }
 ];
 
