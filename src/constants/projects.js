163c8e41346cf39b725c1e4574e68ac3775416e0
김용민 <dydals3440@gmail.com>
2024-04-30 01:30:40 +0900

    feat: 프로젝트 이미지 변경

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -14,7 +14,7 @@ const PROJECTS = [
   {
     id: 2,
     name: 'Waither',
-    description: '분 단위로 날씨 알려주는 나만의 비서, Waither',
+    description: '개개인 맞춤형 날씨 앱 : 나만의 기상비서, Waither',
     theme: ['IOS', 'SPRING'],
     github: 'https://github.com/Waither-Project/Waither-FE',
     year: 3,
@@ -27,7 +27,7 @@ const PROJECTS = [
       '해누/김현우'
     ],
     stack: ['Swift', 'Spring'],
-    img: '/3year/Waither/Waither.png',
+    img: '/3year/Waither/Waither.jpeg',
     release: 'https://www.makeus.in/e046ab3f-95f2-4210-913b-fae4a4251e53'
   },
   {
@@ -35,19 +35,55 @@ const PROJECTS = [
     name: '그로밋',
     description: '깃허브 커밋으로 키우는 나만의 다마고치',
     theme: ['IOS', 'SPRING'],
-    github: null,
+    github: 'https://github.com/TeamGromit',
     year: 3,
-    member: ['조예빈/예콩', '솔솔/이한솔'],
+    member: ['솔솔/이한솔'],
     stack: ['Swift', 'Spring'],
     img: '/3year/다마고치/다마고치.png',
     release: 'https://www.makeus.in/cfe1c32d-89df-43b1-a136-843c3943dbb0'
   },
   {
     id: 4,
+    name: 'Linky-B',
+    description: '마음 맞는 선후배 매칭 서비스',
+    theme: ['ANDROID', 'SPRING'],
+    github: '',
+    year: 3,
+    member: ['퍼플/김동욱', '마크/이기탁', '케빈/김근식'],
+    stack: ['Kotlin', 'Spring'],
+    img: '/3year/링키비/링키비.jpg',
+    release: ''
+  },
+  {
+    id: 5,
+    name: 'CO+DEV',
+    description: '개발자와 디자이너가 모이든 공간, CO+DEV',
+    theme: ['ANDROID', 'SPRING'],
+    github: 'https://github.com/CoDEVumc',
+    year: 3,
+    member: ['앨리/양서영', '환/이환수', '원스/김유정'],
+    stack: ['Kotlin', 'Spring'],
+    img: '/3year/CODEV/CODEV.jpg',
+    release: ''
+  },
+  {
+    id: 6,
+    name: '담소',
+    description: '흡연구역, 더이상 힘들게 찾지 말고 쉽고 간편하게 찾자!',
+    theme: ['IOS', 'SPRING'],
+    github: '',
+    year: 3,
+    member: ['무너/문정호', '레이/윤채영', '민팍/박민희'],
+    stack: ['Swift', 'Spring'],
+    img: '',
+    release: ''
+  },
+  {
+    id: 7,
     name: '동네친구',
     description: '함께 살아가는 자취생활의 힘',
     theme: ['ANDROID', 'SPRING'],
-    github: null,
+    github: 'https://github.com/DongnaeFriend',
     year: 4,
     member: ['링키/윤수빈', '동키/김동현'],
     stack: ['KOTLIN', 'SPRING'],
@@ -55,19 +91,19 @@ const PROJECTS = [
     release: 'https://www.makeus.in/924a9232-80d1-4354-b6e9-b0763b81eb10'
   },
   {
-    id: 5,
+    id: 8,
     name: 'POSESTION',
     description: '뚝딱이들을 위한 사진 포즈 가이드',
     theme: ['ANDROID', 'NODE'],
     github: null,
     year: 4,
-    member: ['코코/최진규'],
+    member: ['원스/김유정', '코코/최진규'],
     stack: ['KOTLIN', 'NODE'],
     img: '/4year/Posestion/Posestion.png',
     release: 'https://www.makeus.in/e41624c5-9fd1-4b8d-be1a-ea1b1105298c'
   },
   {
-    id: 6,
+    id: 9,
     name: 'Pinple',
     description:
       '핫플, 얼마나 많이 붐빌까? 가보기 전에 Pinple로 미리 확인하자!',
@@ -80,19 +116,37 @@ const PROJECTS = [
     release: 'https://www.makeus.in/a9caff6e-8ce7-4a1f-8dad-4f29e3e046fb'
   },
   {
-    id: 7,
+    id: 10,
     name: 'ARMANAGE',
     description: '알바 시간과 급여, 서류 등 이제 한번에 관리, ARMANAGE',
     theme: ['ANDROID', 'SPRING'],
     github: null,
     year: 4,
-    member: ['검정/권혁찬', '이네/설인혜', '윤/권오윤', '제이미/김준환'],
+    member: [
+      '검정/권혁찬',
+      '디채/김채연',
+      '이네/설인혜',
+      '윤/권오윤',
+      '제이미/김준환'
+    ],
     stack: ['KOTLIN', 'SPRING'],
     img: '/4year/ARMANAGE/ARMANAGE.png',
     release: null
   },
   {
-    id: 9,
+    id: 11,
+    name: 'puppyfriend',
+    description: '나의 사랑스러운 댕댕이에게 산책 친구를',
+    theme: ['ANDROID', 'SPRING'],
+    github: 'PuppyFriend',
+    year: 4,
+    member: ['엘레나/권유정', '우현/이현우'],
+    stack: ['KOTLIN', 'SPRING'],
+    img: null,
+    release: null
+  },
+  {
+    id: 12,
     name: '퀸텟:Quintet',
     description: '행복을 위한 5가지 요소 관리앱',
     theme: ['IOS', 'NODE'],
@@ -104,7 +158,7 @@ const PROJECTS = [
     release: 'https://www.makeus.in/4d141c41-5f49-4dc6-8b49-b0c7144ca79a'
   },
   {
-    id: 10,
+    id: 13,
     name: '바른이',
     description: '모든 치아교정 정보를 한 눈에',
     theme: ['IOS', 'SPRING'],
@@ -116,25 +170,19 @@ const PROJECTS = [
     release: 'https://www.makeus.in/4d141c41-5f49-4dc6-8b49-b0c7144ca79a'
   },
   {
-    id: 11,
+    id: 14,
     name: '뮤넥팅(Mu:necting)',
     description: '음악으로 연결하다',
     theme: ['IOS', 'SPRING'],
     github: null,
     year: 4,
-    member: [
-      '깐/김가은',
-      '지토/이현호',
-      '스히/백서희',
-      '사샤/김채연',
-      '한뉴/한유성'
-    ],
+    member: ['깐/김가은', '지토/이현호', '스히/백서희', '한뉴/한유성'],
     stack: ['SWIFT', 'SPRING'],
     img: '/4year/뮤넥팅/뮤넥팅.png',
     release: 'https://www.makeus.in/62b0399d-8986-4097-ab84-0e79f5472d68'
   },
   {
-    id: 12,
+    id: 15,
     name: '홈푸파(홈푸드파이터)',
     description: '가진 것만으로 요리하기',
     theme: ['WEB', 'NODE'],
@@ -154,7 +202,7 @@ const PROJECTS = [
     release: 'https://www.makeus.in/839dd620-1b83-40f5-aa7c-8cc6e4c09e66'
   },
   {
-    id: 13,
+    id: 16,
     name: 'Festie',
     description: '나와 취향이 맞는 친구도 사귀고, 페스티벌도 보러가고!',
     theme: ['WEB', 'SPRING'],
@@ -166,7 +214,7 @@ const PROJECTS = [
     release: 'https://www.makeus.in/b840fac8-6171-430a-9328-f3735352c976'
   },
   {
-    id: 14,
+    id: 17,
     name: 'Healody',
     description: '힐로디로 추적하는 나와 우리 가족의 건강',
     theme: ['WEB', 'SPRING'],
@@ -186,7 +234,19 @@ const PROJECTS = [
     release: 'https://www.makeus.in/3b82ef8f-8398-465f-9dc6-31f383742005'
   },
   {
-    id: 15,
+    id: 18,
+    name: 'TODIS',
+    description: '날씨앱 하나로 옷 코디까지 쉽고 빠르게',
+    theme: ['WEB', 'SPRING'],
+    github: null,
+    year: 4,
+    member: ['블루/최민주', '하비/이승주'],
+    stack: ['REACT', 'SPRING'],
+    img: null,
+    release: 'https://www.makeus.in/7dc138b3-7738-42a9-aa25-a36f85e4b847'
+  },
+  {
+    id: 19,
     name: '여행의 이유',
     description:
       '나를 알고 너를 알고 여행지를 알면, 막막한 여행도 지피지기 백전백승',
@@ -199,7 +259,7 @@ const PROJECTS = [
     release: 'https://www.here-you.com/'
   },
   {
-    id: 16,
+    id: 20,
     name: '고민친구',
     description: '세상의 모든 고민이 거쳐가는 공간, 고민친구',
     theme: ['WEB', 'SPRING'],
@@ -211,7 +271,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 17,
+    id: 21,
     name: 'AboutMe',
     description: '다양한 나, 다양한 관계의 시작',
     theme: ['ANDROID', 'SPRING'],
@@ -223,7 +283,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 18,
+    id: 22,
     name: 'Draw Desktop',
     description:
       '개성 있게 간편하면서 직관적으로 바탕 화면을 꾸밀 수 있는 요소를 제공',
@@ -236,7 +296,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 19,
+    id: 23,
     name: 'Rhythm Palette',
     description: '#음악공유 #이미지생성 #SNS',
     theme: ['WEB', 'SPRING'],
@@ -248,7 +308,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 20,
+    id: 24,
     name: 'Writeroom',
     description: '자유로운 창작의 공간',
     theme: ['WEB', 'SPRING'],
@@ -260,7 +320,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 21,
+    id: 25,
     name: '다온',
     description: '암환자들을 위한 하루 기록 서비스',
     theme: ['ANDROID', 'NODE'],
@@ -272,7 +332,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 22,
+    id: 26,
     name: '더 굿즈',
     description: '개인 판매자 대상 온라인 주문서 플랫폼',
     theme: ['WEB', 'SPRING'],
@@ -284,7 +344,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 23,
+    id: 27,
     name: '아브아브',
     description: '레크레이션 종합 플랫폼 서비스',
     theme: ['WEB', 'SPRING'],
@@ -296,11 +356,11 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 24,
+    id: 28,
     name: '메메',
     description: '나만의 메이크업 메이트',
     theme: ['IOS', 'SPRING'],
-    github: null,
+    github: 'https://github.com/MEME-UMC',
     year: 5,
     member: ['딩동/이동현', '제이스/임재영', '요비/김승엽'],
     stack: ['SWIFT', 'SPRING'],
@@ -308,7 +368,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 25,
+    id: 29,
     name: '마플',
     description: '사용자 저장 장소 기반 기록 + 일정 관리 서비스 : 마플',
     theme: ['IOS', 'NODE'],
@@ -320,11 +380,11 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 26,
+    id: 30,
     name: '스포너스',
     description: '기업과 대학생이 만나는 자리',
     theme: ['IOS', 'SPRING'],
-    github: null,
+    github: 'https://github.com/spon-us',
     year: 5,
     member: ['가니/박가은', '니니/김유빈', '피딕/황인성', '세헌/호세헌'],
     stack: ['SWIFT', 'SPRING'],
@@ -332,7 +392,7 @@ const PROJECTS = [
     release: ''
   },
   {
-    id: 27,
+    id: 31,
     name: '이사.ZIP',
     description: '이사를 위한 정보',
     theme: ['ANDROID', 'SPRING'],
